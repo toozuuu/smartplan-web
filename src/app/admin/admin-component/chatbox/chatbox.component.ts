d3ee711bd034e074a0a745bf5f8048e5f09f47e6
Sachin Dilshan <sachin_n@epiclanka.net>
2021-08-10 14:57:12 +0530

    bugfix

--- src/app/admin/admin-component/chatbox/chatbox.component.ts
+++ src/app/admin/admin-component/chatbox/chatbox.component.ts
@@ -1,15 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {ChatService} from "../../../service/chat.service";
 
 @Component({
   selector: 'app-chatbox',
   templateUrl: './chatbox.component.html',
   styleUrls: ['./chatbox.component.scss']
 })
-export class ChatboxComponent implements OnInit {
+export class ChatboxComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  msgTab = 1;
+  activeChat = 20;
+  isUserSelected = false;
+
+  chatRoomList: any;
+  chatMessagesList: any;
+  chatName: any;
+  displayedChatName: any;
+  displayedImageUrl: any;
+  message: any;
+  selectedChatRoomId: any;
+  selectedUserLastMsgTime: any;
+
+
+  userName: any;
+  uName: any;
+
+  type: any;
+  userType: any;
+
+  landUnit: any;
+  natureOfCultivation: any;
+  name: any;
+
+
+  firstName: any;
+  lastName: any;
+  cityName: any;
+  state: any;
+  postalCode: any;
+
+
+  constructor(private chatService: ChatService) {
+    this.loadChatList();
   }
 
+  loadChatList() {
+    this.chatService.getChatRoomList("ADMIN").subscribe(result => {
+      this.chatRoomList = result;
+    });
+  }
+
+  sendMessageClick() {
+    if (this.message !== undefined && this.message !== null) {
+      let body = {
+        'chatRoomId': this.selectedRoom.chatRoomId,
+        'sender': 'ADMIN',
+        'content': this.message,
+        'receiver': this.selectedRoom.sender,
+      };
+      this.chatService.chat(body).subscribe(() => {
+        this.message = undefined;
+        this.loadChatMessagesList(this.selectedRoom.chatRoomId);
+      });
+    }
+  }
+
+  selectedRoom: any;
+
+  chatRoomClick(room: any, i: number) {
+    this.selectedRoom = room;
+    this.isUserSelected = true;
+    this.loadChatMessagesList(room.chatRoomId);
+  }
+
+  loadChatMessagesList(id) {
+    this.chatService.getChatMessagesList(id).subscribe(result => {
+      this.chatMessagesList = result;
+    });
+  }
 }
