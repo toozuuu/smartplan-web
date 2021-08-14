4275b06b3c438f54df90f072d363d5784b9c15e0
Sachin Dilshan <sachin_n@epiclanka.net>
2021-08-14 18:21:41 +0530

    bugfix

--- src/app/dashboard-main/dashboard-components/product-view/product-view.component.ts
+++ src/app/dashboard-main/dashboard-components/product-view/product-view.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
+import {UserService} from "../../../service/user.service";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-product-view',
@@ -9,17 +11,39 @@ import {ActivatedRoute, Router} from "@angular/router";
 export class ProductViewComponent implements OnInit {
 
   productDetails: any;
+  email: any;
 
   constructor(private router: Router,
               private route: ActivatedRoute,
+              private userService: UserService
   ) {
+    this.email = localStorage.getItem('$email');
   }
 
   ngOnInit(): void {
-
     this.route.queryParams.subscribe(params => {
       this.productDetails = JSON.parse(params.data);
     });
   }
 
+  addToCart() {
+    let body = [{
+      "email": this.email,
+      "unitPrice": this.productDetails.price,
+      "quantity": 1,
+      "mealId": {"id": this.productDetails.id}
+    }];
+
+    this.userService.createCart(body).subscribe((result) => {
+      if (result.success) {
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: 'Item added successfully!',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+    });
+  }
 }
