c8c5ff86624d648b43c6f804b1f6b4c44a992fe8
Sachin Dilshan <sachin_n@epiclanka.net>
2021-09-26 23:07:57 +0530

    bugfix

--- src/app/admin/admin-component/settings/settings.component.ts
+++ src/app/admin/admin-component/settings/settings.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-settings',
@@ -6,11 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
-  currency: any;
+
+  displayedColumns: string[] = ['id', 'description', 'action'];
+  @ViewChild('paginator', {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  dataSource: MatTableDataSource<any>;
+  unitType: any;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  deleteType(id) {
+
+  }
+
+  saveUnitType() {
+
+  }
 }
