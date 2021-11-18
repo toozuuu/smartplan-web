def461a56ed75b8490a12dafa6dd304bb7d341e3
Sachin Dilshan <sachin_n@epiclanka.net>
2021-11-18 22:57:24 +0530

    bugfix

--- src/app/admin/admin-component/admin-setting/admin-setting.component.ts
+++ src/app/admin/admin-component/admin-setting/admin-setting.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from "../../../service/user.service";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-admin-setting',
@@ -19,15 +20,51 @@ export class AdminSettingComponent implements OnInit {
   }
 
   loadAdminDetails() {
+    this.loading();
     this.userService.fetchAllAdminUser().subscribe(result => {
+      Swal.close();
       this.adminEmail = result[0]['email'];
       this.adminPassword = result[0]['password'];
     });
   }
 
   updateAdminLoginDetails() {
-    this.userService.updateAdminDetails({'email':this.adminEmail,'password':this.adminPassword}).subscribe(result=>{
+    Swal.fire({
+      title: 'Admin account details',
+      icon: 'warning',
+      text: 'Do you want to update your account details?',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Confirm'
+    }).then((result) => {
+      if (result.value) {
+        this.loading();
+        this.userService.updateAdminDetails({'email':this.adminEmail,'password':this.adminPassword}).subscribe(result=>{
+          Swal.close();
+          if(result){
+            Swal.fire({
+              icon: 'success',
+              title: 'Updated',
+              text: 'Your account details have been updated!'
+            })
+          }
+        },() => {
+          Swal.close();
+        });
+      }
+    });
 
+  }
+
+  loading() {
+    Swal.fire({
+      title: 'Processing...',
+      allowOutsideClick: false,
+      onBeforeOpen: () => {
+        Swal.showLoading();
+      }
     });
   }
+
 }
